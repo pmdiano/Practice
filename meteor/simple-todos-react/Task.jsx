4c9afef2c32ed8f3133bdf5d0a064c9d78b2956e
pmdiano <pmdiano@gmail.com>
2016-02-14 12:19:06 -0800

    Add MongoDB, toggle checked, remove task

--- meteor/simple-todos-react/Task.jsx
+++ meteor/simple-todos-react/Task.jsx
@@ -2,9 +2,37 @@ Task = React.createClass({
   propTypes: {
     task: React.PropTypes.object.isRequired
   },
+
+  toggleChecked() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this.props.task._id, {
+      $set: {checked: !this.props.task.checked}
+    });
+  },
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  },
+
   render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them nicely in CSS
+    const taskClassName = this.props.task.checked ? "checked" : "";
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked} />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 });
