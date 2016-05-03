f4862aa4e1e0c10db6fde6d9e343c68d5f067099
Qirong Ma <pmdiano@gmail.com>
2016-05-02 20:29:10 -0700

    Basic producer consumer

---
+++ os/producer_consumer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define MAX 10000
+pthread_mutex_t the_mutex;
+pthread_cond_t condc, condp;
+int buffer = 0;
+
+void *producer(void *ptr)
+{
+    int i;
+    for (i = 1; i <= MAX; i++) {
+        pthread_mutex_lock(&the_mutex);
+        while (buffer != 0) pthread_cond_wait(&condp, &the_mutex);
+        buffer = i;
+        printf("Produced cargo %d\n", buffer);
+        pthread_cond_signal(&condc);
+        pthread_mutex_unlock(&the_mutex);
+    }
+    pthread_exit(0);
+}
+
+void *consumer(void *ptr)
+{
+    int i;
+    for (i = 1; i <= MAX; i++) {
+        pthread_mutex_lock(&the_mutex);
+        while (buffer == 0) pthread_cond_wait(&condc, &the_mutex);
+        printf("Consumed cargo %d\n", buffer);
+        buffer = 0;
+        pthread_cond_signal(&condp);
+        pthread_mutex_unlock(&the_mutex);
+    }
+    pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t pro, con;
+    pthread_mutex_init(&the_mutex, 0);
+    pthread_cond_init(&condc, 0);
+    pthread_cond_init(&condp, 0);
+    pthread_create(&con, 0, consumer, 0);
+    pthread_create(&pro, 0, producer, 0);
+    pthread_join(pro, 0);
+    pthread_join(con, 0);
+    pthread_cond_destroy(&condc);
+    pthread_cond_destroy(&condp);
+    pthread_mutex_destroy(&the_mutex);
+}
