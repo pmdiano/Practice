015b3c04cb2cb307f6fe790c98d6d16bce2191ff
Qirong Ma <pmdiano@gmail.com>
2016-12-04 14:56:03 -0800

    practice

---
+++ ruby/snippets.rb
@@ -0,0 +1,10 @@
+# array
+[1].class
+[1].methods.include?(:[])
+
+# hashtable
+numbers = {1 => 'one', 2 => 'two'}
+numbers[1]
+numbers[2]
+stuff = {:array => [1, 2, 3], :string => 'Hi, mon!'}    # symbol
+stuff[:string]
