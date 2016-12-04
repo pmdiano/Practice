0dda229dcff0fa27a07448153eb60d19ec095ae8
Qirong Ma <pmdiano@gmail.com>
2016-12-04 15:20:58 -0800

    prac2

--- ruby/snippets.rb
+++ ruby/snippets.rb
@@ -6,5 +6,16 @@
 numbers = {1 => 'one', 2 => 'two'}
 numbers[1]
 numbers[2]
-stuff = {:array => [1, 2, 3], :string => 'Hi, mon!'}    # symbol
+stuff = {:array => [1, 2, 3], :string => 'Hi, mom!'}    # symbol
 stuff[:string]
+
+# a function example
+def tell_the_truth(options={})
+    if options[:profession] == :lawyer
+        'it could be believed that this is almost certainly not false.'
+    else
+        true
+    end
+end
+tell_the_truth
+tell_the_truth :profession => :lawyer
