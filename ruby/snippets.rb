f774cc5bb3f465e55b3f92119094d28aab4ca9d4
Qirong Ma <pmdiano@gmail.com>
2016-12-26 22:32:25 -0800

    method_missing

--- ruby/snippets.rb
+++ ruby/snippets.rb
@@ -99,3 +99,23 @@ end
 if not obj.respond_to?("to_str")
   raise TypeError, "not a string"
 end
+
+# method_missing to implement Roman number!!
+class Roman
+  def self.method_missing name, *args
+    roman = name.to_s
+    roman.gsub!("IV", "IIII")
+    roman.gsub!("IX", "VIIII")
+    roman.gsub!("XL", "XXXX")
+    roman.gsub!("XC", "LXXXX")
+
+    (roman.count("I") +
+     roman.count("V") * 5 +
+     roman.count("X") * 10 +
+     roman.count("L") * 50 +
+     roman.count("C") * 100)
+  end
+end
+
+puts Roman.X
+puts Roman.XII
